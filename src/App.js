deb81e3db0a9b9ee84b1fc0ae2b57408112d5797
Atangui <48386111+Atangui@users.noreply.github.com>
2020-05-28 02:39:53 +0200

    Premier commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import Insertion from './Components/Insertion';
+
+
+class App extends Component {
+  render() {
+    return (
+      <Insertion/>
+    )
+  }
+}
+
+export default App
